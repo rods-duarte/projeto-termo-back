9e9cf93615570f8c1f9564f012ee3a25ec15cf3a
Rodrigo Duarte Maciel <rods.dduarte@gmail.com>
2022-08-12 17:07:47 -0300

    Fix: send correct error object

--- src/middlewares/validateSchemaMiddleware.ts
+++ src/middlewares/validateSchemaMiddleware.ts
@@ -1,17 +1,22 @@
 import { NextFunction, Request, Response } from 'express';
 import { Schema } from 'joi';
-import { ErrorObject } from './errorHandlerMiddleware.js';
+import { unprocessableEntityError } from './errorHandlerMiddleware.js';
 
 function validateSchema(schema: Schema) {
   return (req: Request, res: Response, next: NextFunction) => {
+    console.log('executou validate');
     const { body } = req;
     const { error } = schema.validate(body);
-    const valid = error === null;
+    console.log(
+      '🚀 ~ file: validateSchemaMiddleware.ts ~ line 10 ~ return ~ error',
+      error
+    );
+    const valid = error == null;
 
     if (!valid) {
       const { details } = error;
       const message = details.map((i) => i.message).join(',');
-      throw { type: 'unprocessableEntity', message } as ErrorObject;
+      throw unprocessableEntityError(message);
     }
 
     next();
